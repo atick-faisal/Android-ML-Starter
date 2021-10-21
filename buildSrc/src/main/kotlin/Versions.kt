39fee2fb2bcb65af6d2a208cd7b62771ea6ecb9e
Atick Faisal <atickfaisal@gmail.com>
2021-10-21 16:13:11 +0600

    Migrate to kotlin dsl

---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,13 @@
+object Versions {
+
+    const val GRADLE_PLUGIN = "7.0.3"
+    const val KOTLIN = "1.5.31"
+
+    const val CORE_KTX = "1.6.0"
+    const val APP_COMPAT = "1.3.1"
+    const val COMPOSE = "1.0.4"
+    const val MATERIAL = "1.4.0"
+    const val LIFECYCLE_KTX = "2.3.1"
+    const val ACTIVITY_COMPOSE = "1.3.1"
+
+}
