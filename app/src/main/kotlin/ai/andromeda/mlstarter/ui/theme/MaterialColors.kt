5ef2de4fafcd6ff3e71faf5b855b96f0b6be1cdf
Atick Faisal <atickfaisal@gmail.com>
2021-10-24 18:57:53 +0600

    Add more material colors

---
+++ app/src/main/kotlin/ai/andromeda/mlstarter/ui/theme/MaterialColors.kt
@@ -0,0 +1,27 @@
+package ai.andromeda.mlstarter.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+object MaterialColors {
+
+    // ... Gray
+    val Gray50 = Color(0xFFFAFAFA)
+
+    // ... Orange
+    val Orange500 = Color(0xFFFF9800)
+    val Orange700 = Color(0xFFF57C00)
+    val Orange900 = Color(0xFFE65100)
+
+    // ...
+    val Purple50 = Color(0xFFF3E5F5)
+    val Purple200 = Color(0xFFBB86FC)
+    val Purple500 = Color(0xFF6200EE)
+    val Purple700 = Color(0xFF3700B3)
+    val Purple900 = Color(0xFF311B92)
+
+    // ... Green
+    val Green500 = Color(0xFF4CAF50)
+    val Green900 = Color(0xFF1B5E20)
+
+    val Teal200 = Color(0xFF03DAC5)
+}
