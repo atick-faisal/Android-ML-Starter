2fdaa28214646122cad75cfea498e648dda22d0e
Atick Faisal <atickfaisal@gmail.com>
2021-10-24 18:54:34 +0600

    Set up colors and themes

--- app/src/main/kotlin/ai/andromeda/mlstarter/ui/theme/Theme.kt
+++ app/src/main/kotlin/ai/andromeda/mlstarter/ui/theme/Theme.kt
@@ -7,15 +7,15 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = MaterialColors.Purple200,
+    primaryVariant = MaterialColors.Purple700,
+    secondary = MaterialColors.Teal200
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = MaterialColors.Purple500,
+    primaryVariant = MaterialColors.Purple700,
+    secondary = MaterialColors.Teal200
 
     /* Other default colors to override
     background = Color.White,
