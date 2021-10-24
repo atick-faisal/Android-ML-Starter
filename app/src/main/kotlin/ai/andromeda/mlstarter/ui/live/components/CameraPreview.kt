f812abad58a098570ee28dd11151785f47fd63a4
Atick Faisal <atickfaisal@gmail.com>
2021-10-24 18:57:02 +0600

    Modify live prediction screen

---
+++ app/src/main/kotlin/ai/andromeda/mlstarter/ui/live/components/CameraPreview.kt
@@ -0,0 +1,63 @@
+package ai.andromeda.mlstarter.ui.live.components
+
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
+import com.orhanobut.logger.Logger
+
+@Composable
+fun CameraPreview(
+    imageAnalyzer: ImageAnalysis,
+    modifier: Modifier = Modifier
+) {
+    val lifecycleOwner = LocalLifecycleOwner.current
+    val context = LocalContext.current
+    val cameraProviderFuture = remember { ProcessCameraProvider.getInstance(context) }
+
+    AndroidView(
+        factory = { ctx ->
+            val previewView = PreviewView(ctx)
+            val executor = ContextCompat.getMainExecutor(ctx)
+
+            cameraProviderFuture.addListener({
+                val cameraProvider = cameraProviderFuture.get()
+                val preview = Preview.Builder().build().also {
+                    it.setSurfaceProvider(previewView.surfaceProvider)
+                }
+
+                val cameraSelector = if (
+                    cameraProvider.hasCamera(CameraSelector.DEFAULT_BACK_CAMERA)
+                ) {
+                    CameraSelector.DEFAULT_BACK_CAMERA
+                } else {
+                    CameraSelector.DEFAULT_FRONT_CAMERA
+                }
+
+                try {
+                    cameraProvider.unbindAll()
+                    cameraProvider.bindToLifecycle(
+                        lifecycleOwner,
+                        cameraSelector,
+                        preview,
+                        imageAnalyzer
+                    )
+                } catch (e: Exception) {
+                    Logger.e("Could not open camera")
+                }
+
+            }, executor)
+            previewView
+        },
+        modifier = modifier.fillMaxSize(),
+    )
+}
