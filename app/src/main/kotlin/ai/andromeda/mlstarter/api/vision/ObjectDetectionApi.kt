11e2ee8384b62da611ad706aba2a7215198c13c2
Atick Faisal <atickfaisal@gmail.com>
2021-10-31 06:10:28 +0600

    Fix issue with 404 not found

--- app/src/main/kotlin/ai/andromeda/mlstarter/api/vision/ObjectDetectionApi.kt
+++ app/src/main/kotlin/ai/andromeda/mlstarter/api/vision/ObjectDetectionApi.kt
@@ -2,12 +2,16 @@ package ai.andromeda.mlstarter.api.vision
 
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
+import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface ObjectDetectionApi {
 
+    @GET("yolo/v5")
+    suspend fun checkServer(): String?
+
     @FormUrlEncoded
-    @POST("/yolo/v5/")
+    @POST("yolo/v5")
     suspend fun analyzeImage(
         @Field("image") image: String
     ): String?
