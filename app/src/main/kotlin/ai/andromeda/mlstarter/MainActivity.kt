ca160a14cf124bf4afad8743f660ddf4ade38e73
Atick Faisal <atickfaisal@gmail.com>
2021-10-31 06:09:41 +0600

    Fix hilt dependency

--- app/src/main/kotlin/ai/andromeda/mlstarter/MainActivity.kt
+++ app/src/main/kotlin/ai/andromeda/mlstarter/MainActivity.kt
@@ -4,7 +4,9 @@ import ai.andromeda.mlstarter.databinding.ActivityMainBinding
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.orhanobut.logger.Logger
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
